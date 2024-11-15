10a8b2e9d348871ebccf1346a78c6d760bfe36fa
Wojciech Miedziak <thyrun@gmail.com>
2024-11-15 22:46:06 +0100

    PZ-14 - added date field to model
    Signed-off-by: Wojciech Miedziak <thyrun@gmail.com>

--- models.py
+++ models.py
@@ -8,6 +8,13 @@ class Currencies(db.Model):
     currency_name = db.Column(db.String(100), nullable=False)
     currency_code = db.Column(db.String(10), nullable=False, unique=True)
     exchange_rate = db.Column(db.Float, nullable=False)
+    date = db.Column(db.Date)
+
+    def __init__(self, currency_name, currency_code, exchange_rate, date):
+        self.currency_name = currency_name
+        self.currency_code = currency_code
+        self.exchange_rate = exchange_rate
+        self.date = date
 
     def __repr__(self):
-        return f'<Waluta {self.code}>'
+        return f"<Waluta {self.currency_name} ({self.currency_code}) dnia {self.date}>"
