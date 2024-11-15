1c9e17b24cba5237878bc070ac2b28e98751d84b
Wojciech Miedziak <thyrun@gmail.com>
2024-11-15 22:10:57 +0100

    PZ-11 - table name fix
    Signed-off-by: Wojciech Miedziak <thyrun@gmail.com>

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Currency(db.Model):
+class Currencies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     currency_name = db.Column(db.String(100), nullable=False)
     currency_code = db.Column(db.String(10), nullable=False, unique=True)
