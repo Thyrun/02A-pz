7a62e588b626d0b822ba6fdd95e4a3e625afbedf
Wojciech Miedziak <thyrun@gmail.com>
2024-11-15 22:42:57 +0100

    PZ-13 - added getting historic data and showing data for single exchange
    Signed-off-by: Wojciech Miedziak <thyrun@gmail.com>

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
@@ -5,9 +6,7 @@ import requests
 from models import db, Currencies
 
 app = Flask(__name__)
-
 app.config.from_pyfile('config.py')
-
 db.init_app(app)
 
 
@@ -24,21 +23,19 @@ def get_currencies():
         response.raise_for_status()
         data = response.json()
 
-        currencies = data[0]['rates']
-        for entry in currencies:
-            currency = Currencies.query.filter_by(currency_code=entry['code']).first()
-            if currency:
-                currency.exchange_rate = entry['mid']
-            else:
-                currency = Currencies(
-                    currency_name=entry['currency'],
-                    currency_code=entry['code'],
-                    exchange_rate=entry['mid']
-                )
-                db.session.add(currency)
+        for currency_data in data[0]['rates']:
+            currency_code = currency_data['code']
+            currency_name = currency_data['currency']
+            exchange_rate = currency_data['mid']
+            date = datetime.now().date()
+
+            existing_entry = Currencies.query.filter_by(currency_code=currency_code, date=date).first()
+            if not existing_entry:
+                new_entry = Currencies(currency_name, currency_code, exchange_rate, date)
+                db.session.add(new_entry)
 
         db.session.commit()
-        return jsonify({"message": "waluty zapisane"})
+        return jsonify({"message": "Waluty zapisane."})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -56,13 +53,52 @@ def test_db():
         result_data = result.fetchone()
 
         if result_data is None:
-            return jsonify({"error": "No result returned from the query"})
+            return jsonify({"error": "Brak rezultatów dla zapytania."})
 
         result_dict = {'test_column': result_data[0]}
 
-        return jsonify({"message": "Bazo widze w tym cloudzie!", "result": result_dict})
+        return jsonify({"message": "Połączenie z bazą prawidłowe", "result": result_dict})
     except Exception as e:
-        return jsonify({"error": f"Fail: {str(e)}"})
+        return jsonify({"error": f"Nieudane połączenie z bazą danych: {str(e)}"})
+
+
+@app.route('/history/<currency_code>')
+def currency_history(currency_code):
+    try:
+        historical_data = Currencies.query.filter_by(currency_code=currency_code).order_by(Currencies.date).all()
+
+        history = [{
+            'date': entry.date,
+            'exchange_rate': entry.exchange_rate
+        } for entry in historical_data]
+
+        return jsonify({'currency_code': currency_code, 'history': history})
+
+    except Exception as e:
+        return jsonify({"error": f"Nie udało się pobrać danych dla {currency_code}: {str(e)}"})
+
+
+@app.route('/get/<release_date>', methods=['GET'])
+def get_legacy_data(release_date):
+    try:
+        response = requests.get("https://api.nbp.pl/api/exchangerates/tables/A/" + release_date + "/?format=json")
+        data = response.json()
+
+        for currency_data in data[0]['rates']:
+            currency_code = currency_data['code']
+            currency_name = currency_data['currency']
+            exchange_rate = currency_data['mid']
+            date = release_date
+
+            existing_entry = Currencies.query.filter_by(currency_code=currency_code, date=date).first()
+            if not existing_entry:
+                new_entry = Currencies(currency_name, currency_code, exchange_rate, date)
+                db.session.add(new_entry)
+
+        db.session.commit()
+        return jsonify({"message": "Waluty zapisane dla daty " + release_date})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
