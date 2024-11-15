58839ac15abd92278dc2dfcfda6c72c6becdd08a
Wojciech Miedziak <thyrun@gmail.com>
2024-11-15 22:55:00 +0100

    PZ-16 - add constraint
    Signed-off-by: Wojciech Miedziak <thyrun@gmail.com>

--- models.py
+++ models.py
@@ -6,9 +6,13 @@ db = SQLAlchemy()
 class Currencies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     currency_name = db.Column(db.String(100), nullable=False)
-    currency_code = db.Column(db.String(10), nullable=False, unique=True)
+    currency_code = db.Column(db.String(10), nullable=False)
     exchange_rate = db.Column(db.Float, nullable=False)
     date = db.Column(db.Date)
+
+    __table_args__ = (
+        db.UniqueConstraint('currency_code', 'date', name='uix_currency_date'),
+    )
 
     def __init__(self, currency_name, currency_code, exchange_rate, date):
         self.currency_name = currency_name
