287cfb2f7e5e544caacb9059f8c052bc3c3ee726
Miedziu <thyrun@gmail.com>
2024-11-30 10:19:37 +0100

    zajecia debug3

--- app.py
+++ app.py
@@ -194,14 +194,7 @@ def convert_currency():
         amount = request.form.get('amount', type=float)
         from_currency = request.form.get('from_currency', type=str).upper()
         to_currency = request.form.get('to_currency', type=str).upper()
-
-        print(f"From Currency: {from_currency}, To Currency: {to_currency}")
-
         date = datetime.now().date()
-
-        print(f"Date: {date}")
-        all_entries = Currencies.query.all()
-        print(all_entries)
 
         if not amount or not from_currency or not to_currency:
             return jsonify({"error": "Podaj poprawne dane: amount, from_currency i to_currency."}), 400
@@ -209,6 +202,9 @@ def convert_currency():
         try:
             from_rate_entry = Currencies.query.filter_by(currency_code=from_currency, date=date).first()
             to_rate_entry = Currencies.query.filter_by(currency_code=to_currency, date=date).first()
+
+            print(f"From Rate Entry: {from_rate_entry}")
+            print(f"To Rate Entry: {to_rate_entry}")
 
             if not from_rate_entry or not to_rate_entry:
                 return jsonify({"error": "Brak danych kursowych dla podanych walut."}), 404
